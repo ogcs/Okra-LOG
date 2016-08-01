d3021a9fb6eaa25b69c6043dcc8376da3c757ee1
tinyz <zou90512@126.com>
2016-08-02 00:28:04 +0800

    Add : 实现简单的Servlet支持

---
+++ src/main/java/org/ogcs/log/serlvet/ApiServlet.java
@@ -0,0 +1,17 @@
+package org.ogcs.log.serlvet;
+
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * Web Servlet.
+ * @author TinyZ
+ * @date 2016-08-01.
+ */
+public interface ApiServlet {
+
+    HttpResponse doGet(HttpRequest request);
+
+    HttpResponse doPost(HttpRequest request);
+
+}
